725232224959c9ff0bb9bf52e2ded82374d60734
Saikumar Mujja <95629853+mujjasaikumar@users.noreply.github.com>
2024-02-19 10:05:32 +0530

    Update twilio_keys.py

--- twilio_keys.py
+++ twilio_keys.py
@@ -1,3 +1,3 @@
-account_sid = 'AC39ccb6704c1ce57e20ccec36bb542e16'
-auth_token = '10c32580b4e631f8c6c5bd5c1d605963'
-twilio_number = '+12292796242'
+account_sid = 'AC39ccb6704c1ce57e20ccec36bb542e16' # sample twilio account ID
+auth_token = '10c32580b4e631f8c6c5bd5c1d605968' # sample authentication token
+twilio_number = '+12292796243' # twilio number through which OTP is sent
